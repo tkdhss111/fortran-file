54d9f90140dcb1558e0510fcac3cdf81f7a259b4
Hisashi Takeda <tkdhss111@gmail.com>
2025-02-19 09:44:22 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,201 @@
+# fortran-file
+
+Fortran Functions for File Manipulation
+
+- Wrapper functions of system operation commands
+
+- Manipulate file or directory with the commands:\
+touch, cp, mv, rm, mkdir, rmdir, cldir, print and find_files
+
+## file_ty
+
+```
+  type file_ty
+
+    character(255) :: path     = ''
+    character(255) :: dir      = ''
+    character(255) :: content  = ''
+    character(50)  :: name     = ''
+    character(50)  :: basename = ''
+    character(10)  :: ext      = ''
+    integer(8)     :: size     = 0
+    logical        :: exist    = .false.
+
+  contains
+
+    procedure :: init  => init_file
+    procedure :: print => print_file
+    procedure :: touch
+    procedure :: rm
+    procedure :: cp
+    procedure :: mv
+    procedure :: mkdir
+    procedure :: rmdir
+    procedure :: cldir
+
+  end type
+```
+
+## Initialization
+
+Initialization process stores file\_ty object with meta data of a file such as:\
+directory name, file name, base name, extension name, file size, and content information.
+
+The derived type member (logical) **exist** is used for the status of actual file existence.
+
+1. Single File Initialization
+
+```
+type(file_ty) :: file1, file2
+
+call file1%init ( path = './dir_a/file_a1.txt' )
+
+! With content option
+call file2%init ( path = './dir_a/file_a2.txt', content = 'Weather Data' )
+```
+
+2. Multiple Files Initialization via File Search
+
+```
+type(file_ty), allocatable :: files(:)
+
+files = find_files ( dir = './dir_a' )
+```
+
+- **find\_files** is a wrapper function of the system operation command "find".
+- Available options are: pattern, ignore, maxdepth, fullpath, type, and image.
+- Use notation "|" for "OR" operator in pattern/ignore option (e.g.,  pattern = '*.txt | *.csv').
+- Use image option for this_image() in coarray multi-threaded environment to avoid filelist file crash.
+
+### Print File
+
+```
+call file1%print
+```
+The above command prints file existence, file size and file path in the following way:
+
+    File/Directory exists: T, Size: 0B, Path: ./dir_test/dir_a/file_a1.txt
+
+### Make Empty File
+
+An empty file on **file_a4%path** is made by the following command:
+
+```
+call touch ( file_a4 )
+```
+or
+```
+call file_a4%touch
+```
+
+**file_a4%exist**(default: .false.) becomes **.true.** after making the file.
+
+    File/Directory exists: T, Size: 0B, Path: ./dir_test/dir_a/file_a1.txt 
+
+### Remove File
+
+A file on **file_a4%path** is removed by the following command:
+
+```
+call rm ( file_a4 )
+```
+or
+```
+call file_a4%rm
+```
+
+**file_a4%exist** becomes **.false.** after the removal of the file.
+
+### Copy File
+
+A file on **file_a1%path** is copied to the place of **file_c1%path** by the following command:
+
+```
+call cp ( file_a1, file_c1 ) 
+```
+or
+```
+call file_a1%cp ( file_c1 ) 
+```
+
+### Move File
+
+A file on **file_a2%path** is moved to the place of **file_c2%path** by the following command:
+
+```
+call mv ( file_a2, file_c2 ) 
+```
+or
+```
+call file_a2%mv ( file_c2 ) 
+```
+
+**file_a2%exist** becomes **.false.** after the file move.
+
+### Make Directory
+
+A directory **dir_f** is created recursively (mkdir -p) by the following command:
+
+```
+type(file_ty) :: dir_f
+call dir_f%init ( path = './dir_f/' ) ! The tailing "/" is required to recognize directory
+```
+
+```
+call mkdir ( dir_f )
+```
+or
+```
+call dir_f%mkdir
+```
+
+### Remove Directory
+
+A directory **dir_f** is removed if it is empty by the following command:
+
+```
+call rmdir ( dir_f )
+```
+or
+```
+call dir_f%rmdir
+```
+
+### Clear Directory
+
+A directory **dir_f** becomes empty by the following command:
+
+```
+call cldir ( dir_f )
+```
+or
+```
+call dir_f%cldir
+```
+
+Only files within the directory are recursively removed.
+
+### Obtain names
+
+Use the following functions:
+
+- dirname for directory name
+- filename for file name with extension
+- basename for file name without extension
+- extname for extension name
+
+```
+dirname  ( './dir_test/dir_a/file_a1.txt' ) ! ./dir_test/dir_a/
+filename ( './dir_test/dir_a/file_a1.txt' ) ! file_a1.txt 
+basename ( './dir_test/dir_a/file_a1.txt' ) ! file_a1
+extname  ( './dir_test/dir_a/file_a1.txt' ) ! .txt
+```
+
+## Test Directory and Files
+
+dir_test\
+├── dir_a\
+│   ├── file_a1.txt\
+│   ├── file_a2.csv\
+│   └── file_a3.html\
+└── dir_b (empty)
---
+++ test/unit_test.f90
@@ -0,0 +1,202 @@
+program unit_test_file_mo
+
+  use file_mo
+
+  implicit none
+
+  type(file_ty) :: file_new, file1, file2, file3
+  type(file_ty) :: dir_a, dir_b, dir_c, dir_d
+  type(file_ty) :: file1_cp, file2_cp, file1_mv, file2_mv
+  type(file_ty), allocatable :: files(:), dirs(:)
+  integer i
+
+  print *, '=========================================='
+  print *, 'Test 1: Utility Functions'
+  print *, '=========================================='
+
+  print *, 'dirname : ', dirname  ( './dir_test/dir_a/file_a1.txt' )
+  print *, 'filename: ', filename ( './dir_test/dir_a/file_a1.txt' )
+  print *, 'basename: ', basename ( './dir_test/dir_a/file_a1.txt' )
+  print *, 'extname : ', extname  ( './dir_test/dir_a/file_a1.txt' )
+
+  print *, '=========================================='
+  print *, 'Test 2: Initialization'
+  print *, '=========================================='
+
+  print *, '------------------------------------------'
+  print *, 'Test 2-1: Basic Initialization'
+  print *, '------------------------------------------'
+
+  call file1%init ( path = './dir_test/dir_a/file_a1.txt'  )
+  call file2%init ( path = './dir_test/dir_a/file_a2.csv'  )
+  call file3%init ( path = './dir_test/dir_a/file_a3.html' )
+
+  call file1%print
+  call file2%print
+  call file3%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 2-2: Initialization via File Search'
+  print *, '------------------------------------------'
+
+  files = find_files ( dir = './dir_test' )
+
+  do i = 1, size(files)
+    call files(i)%print
+  end do
+
+  ! With pattern option (extract text file or HTML file)
+  files = find_files ( dir = './dir_test', pattern = '*.txt|*.html' )
+
+  do i = 1, size(files)
+    call files(i)%print
+  end do
+
+  ! With ignore option (ignore text file or HTML file)
+  files = find_files ( dir = './dir_test', ignore = '*.txt|*.html' )
+
+  do i = 1, size(files)
+    call files(i)%print
+  end do
+
+  ! With type option (find directory only)
+  dirs = find_files ( dir = './dir_test/', type = 'd' )
+
+  do i = 1, size(dirs)
+    call dirs(i)%print
+  end do
+
+  print *, '=========================================='
+  print *, 'Test 3: System Operation Functions'
+  print *, '=========================================='
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-1: touch'
+  print *, '------------------------------------------'
+
+  call touch ( file1 )
+  call file1%print
+stop
+  print *, '------------------------------------------'
+  print *, 'Test 3-2: touch (type-bound procedure)'
+  print *, '------------------------------------------'
+
+  call file2%touch
+  call file2%print
+
+  call file3%touch
+  call file3%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-3: rm'
+  print *, '------------------------------------------'
+
+  call file_new%init ( path = './dir_test/dir_b/file_new.txt' )
+  call file_new%touch
+  call file_new%print
+
+  call rm ( file_new )
+
+  call file_new%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-4: rm (type-bound procedure)'
+  print *, '------------------------------------------'
+
+  call touch ( file_new )
+
+  call file_new%rm
+
+  call file_new%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-5: cp'
+  print *, '------------------------------------------'
+
+  call file1_cp%init ( path = './dir_test/dir_b/file_a1_copy.txt'  )
+
+  call cp ( file1, file1_cp )
+
+  call file1%print
+  call file1_cp%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-6: cp (type-bound procedure)'
+  print *, '------------------------------------------'
+  call file2_cp%init ( path = './dir_test/dir_b/file_a2_copy.txt'  )
+
+  call file2%cp ( file2_cp )
+
+  call file2%print
+  call file2_cp%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-7: mv'
+  print *, '------------------------------------------'
+  call file1_mv%init ( path = './dir_test/dir_b/file_a3_move.html' )
+
+  call mv ( file1, file1_mv )
+
+  call file1%print
+  call file1_mv%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-8: mv (type-bound procedure)'
+  print *, '------------------------------------------'
+  call touch ( file1 )
+  call file2_mv%init ( path = './dir_test/dir_b/file_a3_move2.html' )
+
+  call file1%mv ( file2_mv )
+
+  call file1_mv%print
+  call file2_mv%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-9: mkdir'
+  print *, '------------------------------------------'
+
+  call dir_c%init ( path = './dir_test/dir_c/' )
+
+  call mkdir ( dir_c )
+
+  call dir_c%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-10: mkdir (type-bound procedure)'
+  print *, '------------------------------------------'
+
+  call dir_d%init ( path = './dir_test/dir_d/' )
+
+  call dir_d%mkdir
+
+  call dir_d%print
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-11: rmdir'
+  print *, '------------------------------------------'
+
+  call rmdir ( dir_c )
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-12: rmdir (type-bound procedure)'
+  print *, '------------------------------------------'
+
+  call dir_d%rmdir
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-13: cldir'
+  print *, '------------------------------------------'
+
+  call dir_b%init ( path = './dir_test/dir_b/' )
+  call cldir ( dir_b )
+
+  print *, '------------------------------------------'
+  print *, 'Test 3-13: cldir (type-bound procedure)'
+  print *, '------------------------------------------'
+
+  call touch ( file1_mv )
+  call dir_b%cldir
+
+
+  call touch ( file1 ) ! for recovery to the initial setup
+end program
