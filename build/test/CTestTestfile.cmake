9a786083c26e132ac24d6f851906e8fc1c7f2564
Hisashi Takeda <tkdhss111@gmail.com>
2025-02-19 10:03:27 +0900

    retry

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hss/0_tkd/1_hss/2_tools/fortran-file/test
-# Build directory: /home/hss/0_tkd/1_hss/2_tools/fortran-file/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[unit_test]=] "/home/hss/0_tkd/1_hss/2_tools/fortran-file/build/test/unit_test")
-set_tests_properties([=[unit_test]=] PROPERTIES  WORKING_DIRECTORY "/home/hss/0_tkd/1_hss/2_tools/fortran-file/test" _BACKTRACE_TRIPLES "/home/hss/0_tkd/1_hss/2_tools/fortran-file/test/CMakeLists.txt;10;add_test;/home/hss/0_tkd/1_hss/2_tools/fortran-file/test/CMakeLists.txt;0;")
